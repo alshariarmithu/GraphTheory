9026412f795faf8948bc5c2bdab308784b15ad7d
Al Shariar Hossain <141573403+alshariarmithu@users.noreply.github.com>
2024-10-03 17:19:20 +0600

    Create Cyclic_graph.cpp

---
+++ Cyclic_graph.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 9;
+vector<int>g[N];
+int col[N],par[N];
+bool cycle;
+
+void dfs(int u)
+{
+    col[u]=1;
+    for(auto v:g[u])
+    {
+        if(col[v]==0)
+        {
+            par[v]=u;
+            dfs(v);
+        }
+        else if(col[v]==1)
+        {
+            cycle = true;
+        }
+    }
+    col[u]=2;
+}
+int main()
+{
+    int n,m;cin>>n>>m;
+    while(m--)
+    {
+        int u,v;cin>>u>>v;
+        g[u].push_back(v);
+    }
+    cycle = false;
+    // disconnected graph //
+    for(int i=1;i<=n;i++)
+    {
+        if(col[i]==0)dfs(i);
+    }
+    cout<<cycle<<"\n"; 
+    return 0;
+}
