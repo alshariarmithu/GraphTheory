760d827c70e315a1314cca1326eb9df3a6c4b17a
Al Shariar Hossain <141573403+alshariarmithu@users.noreply.github.com>
2024-10-03 17:14:27 +0600

    Update topSort.cpp

--- topSort.cpp
+++ topSort.cpp
@@ -8,10 +8,6 @@ bool vis[N];
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
     int n, m;
     cin >> n >> m;
     while (m--)
